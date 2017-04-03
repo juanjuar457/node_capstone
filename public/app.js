725697731886199e79c4712307c78d38e85e744d
juanjuar457 <testjj4573421>
2017-04-03 08:22:48 -0700

    node capstone

--- public/app.js
+++ public/app.js
@@ -19,63 +19,6 @@ var state = {
 	requested_materials: []
 
 }
-//loop through units, append an option to the select list
-
-function  init_main_form () {
-
-	for(i=0; i < state.form_data.units.length; i++){
-		$('#units').append('<option value="' + state.form_data.units[i].value +'">' + state.form_data.units[i].name +'</option>') 
-	};	
-
-	$('#add_material').click(add_material_click); 
-
-};
-//this will be moved, login 1st etc.... don't know where quite yet. 
-init_main_form();
-
-//func is like control arm. 
-function add_material_click (event) {
-	event.preventDefault();
-	add_material(); 
-	render_material_list();
-}
-
-function add_material () {. //fill me out!!!!! 
-	var material = {};
-	material.vendor = $('#vendor').val(); //do same for rest, quantity prod cat and units 
-	state.requested_materials.push(material);
-}
-
-function render_material_list () {
-	var dom = $('#requested_materials');
-	dom.empty(); //flushes out material
-
-	for(i=0; i < state.requested_materials; i++){
-		dom.append('<p class="example_entry">'+state.requested_materials[i].product +'/ ct:'+ state.requested_materials[i].quantity +
-		'/'+ state.requested_materials[i].vendor +'/' + state.requested_materials[i].unit_size + state.requested_materials[i].units + '</p>')
-	}
-}
-
-//put together the size and the units glue with css 1 big block...
-
-//NEED TO ADD MOCK DATA HERE AS WELL!!!
-
-
-//on enter keyboard stroke, or Add item will submit!! 
-
-console.log('hello');
-$('.submit_button').click(function () {
-	console.log('test');
-});
-
-
-
-$('form').submit(function(event) {
-	event.preventDefault(); 
-	console.log(prog) //no console log??!?
-	$('p').html("prog");
-});
-
 
 var mock_added_items = {
 	items_ordered: [
@@ -88,18 +31,100 @@ var mock_added_items = {
 		}
 	],
 
-	user: {
-		id: 9181,
-		name: "Rebecca",	
-		type: 'admin' //say admin or guest
-	},
+	user: [
+		{
+			id: 9181,
+			name: "Rebecca",	
+			type: 'admin'
+		}, //say admin or guest
+
+		{
+			id: 1189,
+			name: "Dulcemar",
+			type: 'guest',
+		}
+	],
 
 	company: {
 		id: 7744,
-		name: "Solutions"	
+		name: "Solutions Pharmacy"	
 	}
 }; 
 
+//set up conditional to check for password entered client side in the index/admin.html
+function init_index_form () {
+	if()
+}
+
+
+function  init_main_form () {
+
+	for(i=0; i < state.form_data.units.length; i++){  //sets up the drop down with the units. 
+		$('#units').append('<option value="' + state.form_data.units[i].value +'">' + state.form_data.units[i].name +'</option>') 
+	};	
+
+	$('#add_material').click(add_material_click); 
+
+};
+
+function render_alert_index () {
+
+}
+
+//this will be moved, login 1st etc.... don't know where quite yet. 
+init_main_form();
+
+//func is like control arm basically middle man for functions listed... 
+function add_material_click (event) {
+	event.preventDefault();
+	add_material(); 
+	render_material_list();
+}
+
+function add_material () { 
+	var material = {};	
+	
+	material.product = $('#product').val();
+	state.requested_materials.push(material);
+
+	material.quantity = $('#quantity').val();
+	// state.requested_materials.push(material); OOOOPS
+
+	material.vendor = $('#vendor').val(); //do same for rest, quantity prod cat and units 
+	// state.requested_materials.push(material);
+
+	material.catalog_number = $('#catalog_number').val(); 
+	// state.requested_materials.push(material);
+
+	material.units = $('#units').val();
+
+}
+
+function render_material_list () {
+	var dom = $('#requested_materials');
+	dom.empty(); //flushes out material
+
+	for(i=0; i < state.requested_materials.length; i++){
+		dom.append('<p class="example_entry">'+ state.requested_materials[i].product +'/ count:'+ state.requested_materials[i].quantity + '/' + 
+			state.requested_materials[i].catalog_number + '/'+ state.requested_materials[i].vendor + '/' + state.requested_materials[i].units + '</p>')
+	}
+}
+
+//put together the size and the units glue with css 1 big block...
+
+//NEED TO ADD MOCK DATA HERE AS WELL!!!
+
+//on enter keyboard stroke, or Add item will submit!! 
+
+// console.log('hello');
+// $('.submit_button').click(function () {
+// 	console.log('test');
+// });
+// $('form').submit(function(event) {
+// 	event.preventDefault(); 
+// 	console.log(prog) //no console log??!?
+// 	$('p').html("prog");
+// });
 
 //from thinkful ex on building client with mock data: 
 
--- server.js
+++ server.js
@@ -5,12 +5,6 @@ const morgan = require('morgan');
 
 
 
-// app.get('*', function (req, res) {
-// 	res.json({
-// 		message: 'Hello'
-// 	})
-// });
-
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
